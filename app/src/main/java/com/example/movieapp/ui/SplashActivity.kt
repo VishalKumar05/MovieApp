a7529609d1c92e5350956946afcc222d675e2224
VishalKumar05 <vibrant.vishal75@gmail.com>
2020-02-11 00:03:05 +0530

    Splash work done

--- app/src/main/java/com/example/movieapp/ui/SplashActivity.kt
+++ app/src/main/java/com/example/movieapp/ui/SplashActivity.kt
@@ -1,14 +1,27 @@
 package com.example.movieapp.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import com.example.movieapp.MainActivity
 import com.example.movieapp.R
+import com.example.movieapp.utils.AppConstants
 
 class SplashActivity : AppCompatActivity() {
+
+    private val mHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        mHandler.postDelayed(mRunnable,AppConstants.SPLASH_DELAY)
     }
 
+    private val mRunnable:Runnable = Runnable{
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
+
---
+++ app/src/main/java/com/example/movieapp/utils/AppConstants.kt
@@ -0,0 +1,7 @@
+package com.example.movieapp.utils
+
+class AppConstants {
+    companion object{
+        const val SPLASH_DELAY:Long = 3000
+    }
+}
