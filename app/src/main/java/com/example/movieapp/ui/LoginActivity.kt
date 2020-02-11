5d0b393faa528b127b818a1147ad39b0d9bf1e2e
VishalKumar05 <vibrant.vishal75@gmail.com>
2020-02-12 00:22:11 +0530

    Dummy credential values added

--- app/src/main/java/com/example/movieapp/ui/LoginActivity.kt
+++ app/src/main/java/com/example/movieapp/ui/LoginActivity.kt
@@ -1,16 +1,34 @@
 package com.example.movieapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.movieapp.R
+import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object{
+        private val TAG = LoginActivity.javaClass.simpleName
+        private val DUMMY_CREDENTIALS = arrayOf(
+            "foo@example.com:hello",
+            "admin:admin",
+            "abcde:abcde"
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        
+        login_button.setOnClickListener {
+            performLogin()
+        }
+    }
+
+    private fun performLogin() {
+        Log.d(TAG,"Inside performLogin method")
     }
 
 }
